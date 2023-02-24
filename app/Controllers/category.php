b77f1c3a3c2ab0bffafc18c0cf8184b3fb332b83
Divyrajsinh Vala <divyrajsinh8927@gmail.com>
2023-02-24 18:31:32 +0530

    create category demo page

--- app/Controllers/category.php
+++ app/Controllers/category.php
@@ -1,12 +1,12 @@
 <?php namespace App\Controllers;
 use CodeIgniter\Controller;
  
-class categoryModel extends Controller
+class category extends Controller
 {
     public function index()
     {
         $data['title'] = "Hello World from Codeigniter 4";
-        echo view('hello_view', $data);
+        echo view('category_view', $data);
     }
  
 }
